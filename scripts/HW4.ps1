ca27b7fb6f8c1d8eeaa307962aa2385fc37c7b18
Christopher Ross <cross25@uis.edu>
2017-10-27 11:09:33 -0700

    Initial commit

---
+++ scripts/HW4.ps1
@@ -0,0 +1,55 @@
+﻿<# 
+Christopher Ross
+cross25@uis.edu
+CSC386 HW04
+10/13/2017
+
+PowerShell Homework 04
+This script repeats the project for the Batch file in order to show parameter passing
+and control flow of PowerShell scripts. It also allows for a comparison of language
+features in Windows scripting.
+
+Pseudocode
+    Define parameter names
+
+    If <directory parameter> does not specify a directory
+        display an error message
+        exit the script
+    If the sub-directory <directory parameter>\bob does not exist
+        create the sub-directory
+
+    For each <file> in the directory <directory parameter>
+        If <file> is an image of type <extension parameter>
+            magick <file> -resize 640x <directory parameter>\web-img\_<file>
+#>
+
+Param (
+    [string] $filePath,
+    [string] $fileExt = 'jpg'
+)
+
+if (!(Test-Path $filePath -PathType Container)){
+    Write-Host "ERROR The specified file path doesn't exist" -ForegroundColor Red
+    Break
+}  
+
+# create variable for web-img path
+$imageDir = "$filePath\web-img"
+
+# EXTRA CREDIT: check if web-img folder exists and
+# create it if it doesn't exist
+if (!(Test-Path $imageDir -PathType Container)){
+    New-Item -ItemType directory -Path $imageDir
+}
+
+# resize every file with specified extension and place in web-img folder
+foreach ($img in (get-childitem $filePath\*.$fileExt)) {
+    $currentImg = (Split-Path -Path $img -Leaf)
+    Write-Host "Working on $currentImg"
+    convert $img -resize 640x “$imageDir\$currentImg”
+} 
+
+
+
+
+
---
+++ scripts/PSProject.ps1
@@ -0,0 +1,77 @@
+﻿<# 
+Christopher Ross
+cross25@uis.edu
+CSC386 Powershell Project
+10/25/2017
+
+PowerShell Project
+
+Sources Used:
+
+how to use -fragment to break up html output
+https://stackoverflow.com/questions/16542729/send-multiple-outputs-to-the-same-html-file-in-powershell#16547049
+
+-how to add more css style
+https://technet.microsoft.com/en-us/library/ff730936.aspx
+
+how to return only the value of a property:
+https://www.reddit.com/r/PowerShell/comments/3lyaxt/selectobject_and_return_only_the_value_not_the/
+
+converting bytes to gygabytes
+https://stackoverflow.com/questions/17748325/powershell-converting-from-bytes-into-gigabytes
+
+sorting using the sort-object
+https://technet.microsoft.com/en-us/library/ee176968.aspx
+#>
+
+#create and assign html file to a variable
+$html = "SystemInfo.html"
+
+#set style variables to format html
+$a = "<style>"
+$a = $a + "BODY{background-color:#000033;}"
+$a = $a + "H1{color:white; text-align: center;}"
+$a = $a + "H2{color:#90c8ff;}"
+$a = $a + "TABLE{border-width: 2px;border-style: solid;border-color: black;border-collapse: collapse;}"
+$a = $a + "TH{border-width: 2px;padding: 4px;border-style: solid;border-color: black;background-color:thistle}"
+$a = $a + "TD{border-width: 2px;padding: 4px;border-style: solid;border-color: black;background-color:#e6e6e6}"
+$a = $a + "</style>"
+
+#variables to be displayed in html, many of which use ConvertTo-Html fragments
+$SysName = Get-WmiObject -Class Win32_BIOS | Select -ExpandProperty PSComputerName 
+$Date = Get-Date
+$System = Get-CimInstance -ClassName Win32_ComputerSystem | Select Manufacturer, Model, 
+                SystemType, TotalPhysicalMemory, Domain | ConvertTo-Html -As Table -Fragment
+$Boot = Get-WmiObject -Class Win32_BootConfiguration | Select BootDirectory, 
+                Description |  ConvertTo-Html -As Table -Fragment
+$Bios = Get-CimInstance -ClassName Win32_BIOS | Select Manufacturer, SMBIOSBIOSVersion, 
+                Version, SerialNumber, ReleaseDate | ConvertTo-Html -As Table -Fragment
+$OS = Get-WmiObject -Class Win32_OperatingSystem | Select Caption, Version, BuildNumber, 
+                OSArchitecture, OSLanguage | ConvertTo-Html -As Table -Fragment
+$TimeZone = Get-WmiObject -Class Win32_TimeZone | Select StandardName, Caption | 
+                ConvertTo-Html -As Table -Fragment
+$LogicalDisk = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType='3'" | Select Caption, Description, 
+                FileSystem, @{n='FreeSpace(GB)';e={[int]($_.FreeSpace/1GB)}},
+                @{n='Size(GB)';e={[int]($_.Size/1GB)}} | ConvertTo-Html -As Table -Fragment
+$Processor = Get-WmiObject -Class Win32_Processor | select Name, Manufacturer, NumberOfCores,ThreadCount, 
+                MaxClockSpeed, CurrentClockSpeed | ConvertTo-Html -As Table -Fragment
+$Memory = Get-WmiObject -Class Win32_PhysicalMemory  | select Tag, Manufacturer, SerialNumber, PartNumber,
+                @{n='Capacity(GB)';e={[int]($_.Capacity/1GB)}}, DeviceLocator | 
+                ConvertTo-Html -As Table -Fragment
+$Product = Get-WmiObject -Class Win32_Product | Select Vendor, Name, Version, Caption | Sort-Object Vendor |
+                ConvertTo-Html -As Table -Fragment
+
+#Converts all the Html fragments generated above into a single html document with appropriate headers
+ConvertTo-HTML -head $a  -Body "<H1>$SysName $Date</h1> 
+    <H2>Computer System Information</H2> $System
+    <H2>Boot Partition Information</H2> $Boot
+    <H2>System BIOS Information</H2> $Bios
+    <H2>Operating System Information</H2> $OS
+    <H2>Time Zone Data</H2> $TimeZone
+    <H2>Logical Disk Information</H2> $LogicalDisk
+    <H2>System Processor Information</H2> $Processor
+    <H2>Physical Memory</H2> $Memory
+    <H2>Installed Software Inventory</H2> $Product" | Out-File $html
+
+#Open the html file to display results
+ii $html
